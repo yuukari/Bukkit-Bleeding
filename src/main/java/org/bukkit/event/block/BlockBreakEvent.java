1dd3393f39beb70b40d5e642160588ed098a8a03
Celtic Minstrel <celtic.minstrel.ca@some.place>
2011-08-14 22:34:13 -0400

    Interface for getting and setting drops in a manner analogous to the method used in entity death

--- src/main/java/org/bukkit/event/block/BlockBreakEvent.java
+++ src/main/java/org/bukkit/event/block/BlockBreakEvent.java
@@ -1,9 +1,13 @@
 package org.bukkit.event.block;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * Called when a block is broken by a player.
@@ -19,8 +23,9 @@ public class BlockBreakEvent extends BlockEvent implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
     private final Player player;
     private boolean cancel;
+    private List<ItemStack> drops;
 
-    public BlockBreakEvent(final Block theBlock, final Player player) {
+    public BlockBreakEvent(final Block theBlock, final Player player, List<ItemStack> drops) {
         super(theBlock);
         this.player = player;
         this.cancel = false;
@@ -33,6 +38,14 @@ public class BlockBreakEvent extends BlockEvent implements Cancellable {
      */
     public Player getPlayer() {
         return player;
+    }
+    
+    public List<ItemStack> getDrops() {
+        return drops;
+    }
+    
+    public void setDrops(List<ItemStack> drops) {
+        this.drops = drops;
     }
 
     public boolean isCancelled() {
