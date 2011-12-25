dbbc2533614b3efd7df87c9270fc814ff8aacbf5
Erik Broes <erikbroes@grum.nl>
2011-12-25 12:53:58 +0100

    Fixed up codestyle, still not 100% happy.

--- src/main/java/org/bukkit/plugin/java/JavaPlugin.java
+++ src/main/java/org/bukkit/plugin/java/JavaPlugin.java
@@ -150,36 +150,31 @@ public abstract class JavaPlugin implements Plugin {
     }
 
     public void saveResource(String resourcePath, boolean replace) {
-        if(resourcePath == null || resourcePath.equals("")) {
+        if (resourcePath == null || resourcePath.equals("")) {
             throw new IllegalArgumentException("ResourcePath cannot be null or empty");
         }
 
         resourcePath = resourcePath.replace('\\', '/');
         InputStream in = getResource(resourcePath);
-        if(in == null) {
+        if (in == null) {
             throw new IllegalArgumentException("The embedded resource '" + resourcePath + "' cannot be found in " + getFile());
         }
 
-        File outFile = new File(getDataFolder(), resourcePath);  
+        File outFile = new File(getDataFolder(), resourcePath);
         int lastIndex = resourcePath.lastIndexOf('/');
         File outDir = new File(getDataFolder(), resourcePath.substring(0, lastIndex >= 0 ? lastIndex : 0));
-        
-        
-        if(!outDir.exists()) {
+
+        if (!outDir.exists()) {
             outDir.mkdirs();
         }
 
-        if(in == null) {
-            in = new ByteArrayInputStream(new byte[0]);
-        }
-
         try {
-            if(!outFile.exists() || replace) {
+            if (!outFile.exists() || replace) {
                 OutputStream out = new FileOutputStream(outFile);
                 byte[] buf = new byte[1024];
                 int len;
-                while((len=in.read(buf))>0) {
-                    out.write(buf,0,len);
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
                 }
                 out.close();
                 in.close();
