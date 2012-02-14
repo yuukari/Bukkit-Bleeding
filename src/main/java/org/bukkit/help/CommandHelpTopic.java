c9d9f184fabb0bc028a757d75ebeaabd66d85989
rmichela <deltahat@gmail.com>
2012-02-13 21:26:08 -0500

    More work on Help

--- src/main/java/org/bukkit/help/CommandHelpTopic.java
+++ src/main/java/org/bukkit/help/CommandHelpTopic.java
@@ -1,23 +1,34 @@
 package org.bukkit.help;
 
+import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
+import org.apache.commons.lang.StringUtils;
 
-/**
- */
 public class CommandHelpTopic implements HelpTopic {
+
+    private Command command;
+
+    public CommandHelpTopic(Command command) {
+        this.command = command;
+    }
+
     public boolean playerCanSee(Player player) {
-        return false;  //To change body of implemented methods use File | Settings | File Templates.
+        return player.hasPermission(command.getPermission());
     }
 
     public String getName() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return command.getLabel();
     }
 
     public String getShortText() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        if (command.getAliases().size() > 0) {
+            return command.getUsage() + "\nAliases: " + StringUtils.join(command.getAliases(), ", ");
+        } else {
+            return command.getUsage();
+        }
     }
 
     public String getFullText() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return command.getDescription();
     }
 }
--- src/main/java/org/bukkit/help/HelpTopic.java
+++ src/main/java/org/bukkit/help/HelpTopic.java
@@ -3,10 +3,32 @@ package org.bukkit.help;
 import org.bukkit.entity.Player;
 
 /**
+ * HelpTopic implementations are displayed to the user when the user uses the /help command.
  */
 public interface HelpTopic {
+    /**
+     * Determines if a {@link Player} is allowed to see this help topic.
+     * @param player The Player in question.
+     * @return True of the Player can see this help topic, false otherwise.
+     */
     boolean playerCanSee(Player player);
+
+    /**
+     * Returns the name of this help topic. Must not contain spaces.
+     * @return The topic name.
+     */
     String getName();
+
+    /**
+     * Returns a brief description that will be displayed in the topic index.
+     * @return A brief topic description.
+     */
     String getShortText();
+
+    /**
+     * Returns the full description of this help topic that is displayed when the user requests this topic's details.
+     * The result will be paginated to properly fit the user's client.
+     * @return
+     */
     String getFullText();
 }
