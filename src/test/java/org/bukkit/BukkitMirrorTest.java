5e9d98cfa8d8fefca4685406644ec87979a26398
Celtic Minstrel <celtic.minstrel.ca@some.place>
2012-02-25 12:52:28 -0500

    Remove unused method stubs

--- src/test/java/org/bukkit/BukkitMirrorTest.java
+++ src/test/java/org/bukkit/BukkitMirrorTest.java
@@ -5,30 +5,9 @@ import static org.junit.Assert.*;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class BukkitMirrorTest {
-
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {
-    }
-
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
-
     @Test
     public final void test() throws NoSuchMethodException {
         Method[] serverMethods = Server.class.getDeclaredMethods();
@@ -37,5 +16,4 @@ public class BukkitMirrorTest {
             assertTrue("Bukkit." + method.getName() + " must be static!", Modifier.isStatic(mirrorMethod.getModifiers()));
         }
     }
-
 }
